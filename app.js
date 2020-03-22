bb0222196de81a9c5478e18a257b843bf2bfa084
David Sucur <davidsucur111@gmail.com>
2020-03-22 23:55:23 +0100

    Inital routes

--- app.js
+++ app.js
@@ -1,6 +1,20 @@
 const express = require("express");
 
+const cpu = require("./routes/cpu");
+const gpu = require("./routes/gpu");
+const motherboard = require("./routes/motherboard");
+const powersupply = require("./routes/powersupply");
+const ram = require("./routes/ram");
+const ssd = require("./routes/ssd");
+
 const app = express();
+
+app.use("/api/cpu", cpu);
+app.use("/api/gpu", gpu);
+app.use("/api/motherboard", motherboard);
+app.use("/api/powersupply", powersupply);
+app.use("/api/ram", ram);
+app.use("/api/ssd", ssd);
 
 app.get("/", (req, res) => {
   res.send("<h1>HOLA AMIGO</h1>");
--- server.js
+++ server.js
@@ -20,7 +20,7 @@ try {
   console.log(e);
 }
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
   console.log("Server is running...");

f9bff30977c38e5c558d617c160670f3cb9d7d72
David <davidsucur111@gmail.com>
2020-03-24 21:25:43 +0100

    body parses middleware

--- app.js
+++ app.js
@@ -2,12 +2,15 @@ const express = require("express");
 
 const cpu = require("./routes/cpu");
 const gpu = require("./routes/gpu");
-const motherboard = require("./routes/motherboard");
-const powersupply = require("./routes/powersupply");
 const ram = require("./routes/ram");
 const ssd = require("./routes/ssd");
+const motherboard = require("./routes/motherboard");
+const powersupply = require("./routes/powersupply");
 
 const app = express();
+
+// Bodyparses middleware
+app.use(express.json({extended:true}))
 
 app.use("/api/cpu", cpu);
 app.use("/api/gpu", gpu);
